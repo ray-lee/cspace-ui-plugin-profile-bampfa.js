1277d1bab51ac53313751b76cc65d60b237aa675
Ray Lee <rhlee@berkeley.edu>
2017-08-02 19:08:49 -0700

    BAMPFA-537: Change login page text.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import messages from './messages';
 import styles from '../styles/cspace-ui-plugin-profile/bampfa.css';
 
 module.exports = () => ({
+  messages,
   className: styles.common,
 });
---
+++ src/messages.js
@@ -0,0 +1,12 @@
+export default {
+  'about.title': 'CollectionSpace',
+  'about.content': `
+    <p>
+      Collections management system of the UC Berkeley Art Museum and Pacific Film Archive.
+    </p>
+    <p>
+      This service is supported by UC Berkeley <a href="http://research-it.berkeley.edu/">Research IT</a>.
+      To send feedback, report bugs, or request support, email
+      <a href="mailto:cspace-support@lists.berkeley.edu">cspace-support@lists.berkeley.edu</a>.
+    </p>`,
+};
