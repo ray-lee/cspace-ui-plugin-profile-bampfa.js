0a67dc9247a851e3c6aed5a076f51d207805f81d
Ray Lee <rhlee@berkeley.edu>
2017-08-02 19:00:16 -0700

    BAMPFA-538: Create skeletal BAMPFA profile plugin with no configuration.

---
+++ README.md
@@ -0,0 +1,3 @@
+# cspace-ui-plugin-profile-bampfa
+
+A cspace-ui plugin containing configuration for the UC Berkeley Art Museum and Pacific Film Archive.
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+/* eslint import/no-extraneous-dependencies: "off" */
+
+const webpack = require('webpack');
+const path = require('path');
+
+const library = 'cspaceUIPluginProfileBAMPFA';
+const isProduction = process.env.NODE_ENV === 'production';
+const filename = `${library}${isProduction ? '.min' : ''}.js`;
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    filename,
+    library,
+    libraryTarget: 'umd',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[folder]-[name]--[local]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
+
+if (isProduction) {
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin());
+}
+
+module.exports = config;
