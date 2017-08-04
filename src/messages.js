30ddea2ef61c38d96868b09849ce704cceb70b9b
Ray Lee <rhlee@berkeley.edu>
2017-08-03 22:07:29 -0700

    BAMPFA-539,BAMPFA-540: Update logo, tweak login page text.

--- src/messages.js
+++ src/messages.js
@@ -1,12 +1,9 @@
 export default {
-  'about.title': 'CollectionSpace',
+  'about.title': 'CollectionSpace - BAMPFA',
   'about.content': `
     <p>
-      Collections management system of the UC Berkeley Art Museum and Pacific Film Archive.
-    </p>
-    <p>
-      This service is supported by UC Berkeley <a href="http://research-it.berkeley.edu/">Research IT</a>.
-      To send feedback, report bugs, or request support, email
+      This service is supported by UC Berkeley <a href="http://research-it.berkeley.edu/">Research
+      IT</a>. To send feedback, report bugs, or request support, email
       <a href="mailto:cspace-support@lists.berkeley.edu">cspace-support@lists.berkeley.edu</a>.
     </p>`,
 };
