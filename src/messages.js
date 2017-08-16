7549988803ee579add0040e6fd964e9773c2befd
Cesar Villalobos Huizar <cesar@Cesars-MacBook-Pro.local>
2017-08-16 01:57:22 -0700

    BAMPFA-546: Changed messages of fields

--- src/messages.js
+++ src/messages.js
@@ -2,8 +2,19 @@ export default {
   'about.title': 'CollectionSpace - BAMPFA',
   'about.contentHTML': `
     <p>
-      This service is supported by UC Berkeley <a href="http://research-it.berkeley.edu/">Research
-      IT</a>. To send feedback, report bugs, or request support, email
+      Collections management system of the UC Berkeley Art Museum and Pacific Film Archive.
+    </p>
+    <p>
+      This service is supported by UC Berkeley <a href="http://research-it.berkeley.edu/">Research IT</a>.
+      To send feedback, report bugs, or request support, email
       <a href="mailto:cspace-support@lists.berkeley.edu">cspace-support@lists.berkeley.edu</a>.
     </p>`,
+    'field.intakes_common.entryNumber.name': 'TR Number',
+    'field.intakes_common.entryNote.name': 'Intake note',
+    'field.intakes_common.packingNote.name': 'Disposition',
+    'field.intakes_common.entryReason.name': 'Purpose',
+    'field.intakes_common.currentOwner.name': 'Source',
+    'field.intakes_common.currentLocation.name': 'Current location',
+    'field.intakes_common.currentLocationFitness.name': 'Current location fitness',
+    'field.intakes_common.currentLocationNote.name': 'Current location note',
 };
