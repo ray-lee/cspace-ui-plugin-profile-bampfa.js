4e2db0a9824de3bf861860093550d7f636c091ab
Cesar Villalobos <cesarv.h@berkeley.edu>
2017-10-31 16:10:08 -0700

    BAMPFA-546: Fixed entryReason source in fields.js

--- src/recordTypes/intake/fields.js
+++ src/recordTypes/intake/fields.js
@@ -127,7 +127,7 @@ export default (pluginContext) => {
             view: {
               type: OptionPickerInput,
               props: {
-                source: 'entryReasons',
+                source: 'intakepurpose',
               },
             },
           },
