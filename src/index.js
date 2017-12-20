cca02608fc6fad4db952d4f08daf5d8a697c65a0
Ray Lee <rhlee@berkeley.edu>
2017-12-19 23:22:23 -0800

    BAMPFA-552: Move record disabling into the plugin.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,27 @@ import messages from './messages';
 import recordTypes from './recordTypes';
 import styles from '../styles/cspace-ui-plugin-profile/bampfa.css';
 
-
-module.exports = () => (pluginContext) => {
-  const result = recordTypes(pluginContext).reduce((config, plugin) => merge(config, plugin), {
-    messages,
-    className: styles.common,
-  });
-  return result;
-};
+module.exports = () => pluginContext =>
+  recordTypes(pluginContext).reduce(
+    (config, configContribution) => merge(config, configContribution), {
+      messages,
+      className: styles.common,
+      recordTypes: {
+        acquisition: {
+          disabled: true,
+        },
+        claim: {
+          disabled: true,
+        },
+        conservation: {
+          disabled: true,
+        },
+        exhibition: {
+          disabled: true,
+        },
+        objectexit: {
+          disabled: true,
+        },
+      },
+    }
+  );
