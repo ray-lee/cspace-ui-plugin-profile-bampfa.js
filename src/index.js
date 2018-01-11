081076d09055bc50e7216a50be8ec28a4bed75ba
Ray Lee <rhlee@berkeley.edu>
2018-01-10 16:20:30 -0800

    NOJIRA: Upgrade cspace-ui version, config reorganization.

--- src/index.js
+++ src/index.js
@@ -1,29 +1,18 @@
 import merge from 'lodash/merge';
+
 import messages from './messages';
 import recordTypes from './recordTypes';
+import logo from '../images/logo.svg';
 import styles from '../styles/cspace-ui-plugin-profile/bampfa.css';
 
-module.exports = () => pluginContext =>
-  recordTypes(pluginContext).reduce(
-    (config, configContribution) => merge(config, configContribution), {
-      messages,
-      className: styles.common,
-      recordTypes: {
-        acquisition: {
-          disabled: true,
-        },
-        claim: {
-          disabled: true,
-        },
-        conservation: {
-          disabled: true,
-        },
-        exhibition: {
-          disabled: true,
-        },
-        objectexit: {
-          disabled: true,
-        },
-      },
-    }
-  );
+module.exports = () => pluginContext => merge(
+  {
+    logo,
+    messages,
+    className: styles.common,
+    prettyUrls: true,
+    serverTimeZone: 'America/Los_Angeles',
+    tenantId: '55',
+  },
+  recordTypes(pluginContext),
+);
--- src/recordTypes/index.js
+++ src/recordTypes/index.js
@@ -1,29 +1,51 @@
+import merge from 'lodash/merge';
+
+import citation from './citation';
+import collectionobject from './collectionobject';
+import concept from './concept';
+import conditioncheck from './conditioncheck';
 import group from './group';
 import intake from './intake';
-import movement from './movement';
-import collectionobject from './collectionobject';
-import conditioncheck from './conditioncheck';
-import media from './media';
-import person from './person';
-import organization from './organization';
 import location from './location';
-import work from './work';
+import media from './media';
+import movement from './movement';
+import organization from './organization';
+import person from './person';
 import place from './place';
-import concept from './concept';
-import citation from './citation';
+import work from './work';
 
 export default pluginContext => [
-  group(pluginContext),
-  intake(pluginContext),
-  collectionobject(pluginContext),
-  movement(pluginContext),
-  conditioncheck(pluginContext),
-  media(pluginContext),
-  person(pluginContext),
-  organization(pluginContext),
-  location(pluginContext),
-  work(pluginContext),
-  place(pluginContext),
-  concept(pluginContext),
-  citation(pluginContext),
-];
+  citation,
+  collectionobject,
+  concept,
+  conditioncheck,
+  group,
+  intake,
+  location,
+  media,
+  movement,
+  organization,
+  person,
+  place,
+  work,
+]
+.map(configurer => configurer(pluginContext))
+.reduce(merge, {
+  recordTypes: {
+    acquisition: {
+      disabled: true,
+    },
+    claim: {
+      disabled: true,
+    },
+    conservation: {
+      disabled: true,
+    },
+    exhibition: {
+      disabled: true,
+    },
+    objectexit: {
+      disabled: true,
+    },
+  },
+});
