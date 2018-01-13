3a08136c62017af55acc993cbc47b9ac33d66500
Cesar Villalobos <cesarv.h@berkeley.edu>
2018-01-12 22:00:29 -0800

    BAMPFA-544: Fixed edge cases in FML/LMF fields

--- src/recordTypes/person/fields.js
+++ src/recordTypes/person/fields.js
@@ -20,20 +20,98 @@ export default (pluginContext) => {
         personTermGroupList: {
           personTermGroup: {
             [config]: {
-              compute: (data) => {
-                const forename = data.get('foreName');
-                const middle = data.get('middleName');
-                const surname = data.get('surName');
-                const additions = data.get('nameAdditions');
+              compute: (data, path, recordData) => {
+                let foreName = data.get('foreName');
+                let middleName = data.get('middleName');
+                let surName = data.get('surName');
+                const nameAdditions = data.get('nameAdditions');
+                const nationalities = recordData.getIn(['document', 'ns2:persons_common', 'nationalities', 'nationality']);
+                const title = data.get('title');
 
-                const termDisplayName = [surname, forename, middle, additions].filter(part => !!part).join(', ');
-                const termName = [forename, middle, surname, additions].filter(part => !!part).join(' ');
+                let nationality = '';
+
+                const namePartsFML = [];
+                const namePartsLMF = [];
+
+                /* Calculate First-Middle-Last Name */
+                if (nationalities && nationalities.size > 0) {
+                  for (const candidateNationality of nationalities) {
+                    if (candidateNationality !== '' && candidateNationality !== undefined) {
+                      nationality = candidateNationality;
+                      break;
+                    }
+                  }
+                }
+                if (surName && surName.toLowerCase() === 'unknown') {
+                  namePartsFML.push(surName);
+                  if (nationality) {
+                    namePartsFML.push(`(${nationality})`);
+                  }
+                } else {
+                  if (title) {
+                    namePartsFML.push(title);
+                  }
+                  if (foreName) {
+                    namePartsFML.push(foreName);
+                  }
+                  if (middleName) {
+                    namePartsFML.push(middleName);
+                  }
+                  if (surName) {
+                    namePartsFML.push(surName);
+                  }
+                  if (nameAdditions) {
+                    namePartsFML.push(nameAdditions);
+                  }
+                }
+                /* Calculate Last-middleName-First Name */
+                if (surName && surName.toLowerCase() === 'unknown') {
+                  namePartsLMF.push(surName);
+                  if (nationality) {
+                    namePartsLMF.push(`(${nationality})`);
+                  }
+                } else if (!surName) {
+                  namePartsLMF.push(foreName);
+                } else if (!foreName) {
+                  namePartsLMF.push(surName);
+                } else if (!middleName) {
+                  if (nationality && nationality.toLowerCase().indexOf('china') < 0) {
+                    surName = `${surName},`;
+                  }
+                  namePartsLMF.push(surName);
+
+                  if (nameAdditions) {
+                    foreName = `${foreName},`;
+                  }
+                  namePartsLMF.push(foreName);
+
+                  if (nameAdditions) {
+                    namePartsLMF.push(nameAdditions);
+                  }
+                } else {
+                  if (nationality && nationality.toLowerCase().indexOf('china') < 0) {
+                    surName = `${surName},`;
+                  }
+                  namePartsLMF.push(surName);
+                  namePartsLMF.push(foreName);
+
+                  if (nameAdditions) {
+                    middleName = `${middleName},`;
+                  }
+
+                  namePartsLMF.push(middleName);
+
+                  if (nameAdditions) {
+                    namePartsLMF.push(nameAdditions);
+                  }
+                }
+                const nameLMFName = namePartsLMF.join(' ');
+                const nameFMLName = namePartsFML.join(' ');
 
                 return Immutable.Map({
-                  termDisplayName,
-                  termName,
+                  termDisplayName: nameLMFName,
+                  termName: nameFMLName,
                 });
-                // return data.set(['termName', 'termDisplayName'], displayName);
               },
             },
           },
