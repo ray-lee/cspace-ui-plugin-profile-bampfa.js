4f6e8dc63ed265f1085503a6fb005e692de7ce1c
Cesar Villalobos <cesarv.h@berkeley.edu>
2018-01-15 18:01:00 -0800

    BAMPFA-547: Done with computed dimensionsSummary field

--- src/recordTypes/media/fields.js
+++ src/recordTypes/media/fields.js
@@ -13,6 +13,10 @@ export default (pluginContext) => {
   } = pluginContext.configHelpers;
 
   const {
+    Immutable,
+  } = pluginContext.lib;
+
+  const {
     DATA_TYPE_INT,
     DATA_TYPE_BOOL,
   } = pluginContext.dataTypes;
@@ -37,6 +41,13 @@ export default (pluginContext) => {
             },
           },
           measuredPartGroup: {
+            measuredPart: {
+              [config]: {
+                view: {
+                  type: TextInput,
+                },
+              },
+            },
             dimensionSummary: {
               [config]: {
                 view: {
@@ -44,6 +55,111 @@ export default (pluginContext) => {
                   props: {
                     readOnly: true,
                   },
+                },
+                compute: (data, path, recordData) => {
+                  const measurements = {};
+                  const pathCopy = Object.assign([], path);
+
+                  /* Perform a series of pops and pushes to a path to get the correct values */
+                  pathCopy.pop();
+                  pathCopy.push('measuredPart');
+                  const measuredPart = recordData.getIn(pathCopy);
+                  pathCopy.pop();
+                  const dimensionSubGroup = recordData.getIn(pathCopy);
+                  pathCopy.push('measuredPartNote');
+                  const measuredPartNote = recordData.getIn(pathCopy);
+
+
+                  // Collect the necessary measurements for the summary.
+
+                  // Measurements with units other than inches and centimeters are excluded.
+
+                  // Measurements with empty values are excluded.
+
+                  // A dimension could be measured more than once.
+                  // If this happens, the first (top-most) measurement
+                  // of that dimension is used. All others are excluded.
+
+                  for (let i = 0; i < dimensionSubGroup.length; i += 1) {
+                    const measurement = dimensionSubGroup[i];
+                    const dimension = measurement.dimension;
+                    const val = measurement.value;
+                    const unt = measurement.measurementUnit;
+
+                    if ((unt === 'in' || unt === 'cm.') && val != null && val !== '' && !(dimension in measurements)) {
+                      measurements[dimension] = {
+                        value: val,
+                        unit: unt,
+                      };
+                    }
+                  }
+
+                  // Order the collected measurements by dimension, and drop measurements of
+                  // dimensions that are not used in the summary.
+
+                  const orderedDimensions = ['height', 'width', 'depth', 'diameter'];
+                  const orderedMeasurements = [];
+                  const usedUnits = {};
+
+                  for (let i = 0; i < orderedDimensions.length; i += 1) {
+                    const dimension = orderedDimensions[i];
+
+                    if (dimension in measurements) {
+                      const measurement = measurements[dimension];
+
+                      orderedMeasurements.push(measurement);
+                      usedUnits[measurement.unit] = true;
+                    }
+                  }
+
+                  // Create descriptions of each measurement. If all measurements share
+                  // a common unit, this is just the value of the measurement. Otherwise,
+                  // it's the value and the unit.
+
+                  const orderedMeasurementDescriptions = [];
+                  const hasCommonUnit = (Object.keys(usedUnits).length === 1);
+
+                  for (let i = 0; i < orderedMeasurements.length; i += 1) {
+                    const measurement = orderedMeasurements[i];
+                    let measurementDescription = measurement.value;
+
+                    if (!hasCommonUnit) {
+                      measurementDescription = `${measurementDescription} ${measurement.unit}`;
+                    }
+
+                    orderedMeasurementDescriptions.push(measurementDescription);
+                  }
+
+                  // Join all measurement descriptions with x
+                  let measurementSummary = orderedMeasurementDescriptions.join(" x ");
+
+                  // If there is a common unit, append it
+                  if (hasCommonUnit) {
+                    const commonUnit = (Object.keys(usedUnits))[0];
+                    measurementSummary = `${measurementSummary} ${commonUnit}`;
+                  }
+
+                  // Compose this with the measured part and the measured part note
+
+                  const summaryParts = [];
+
+                  if (measuredPart && measuredPart !== '') {
+                    summaryParts.push(`${measuredPart}:`);
+                  }
+
+                  if (measurementSummary !== '') {
+                    summaryParts.push(measurementSummary);
+                  }
+
+                  if (measuredPartNote && measuredPartNote !== '') {
+                    summaryParts.push(`(${measuredPartNote})`);
+                  }
+
+                  const joinedParts = summaryParts.join(' ');
+                  // return Immutable.Map({
+                    // dimensionSummary: joinedParts,
+                  // });
+                  return joinedParts;
                 },
               },
             },
@@ -120,7 +236,7 @@ export default (pluginContext) => {
             messages: defineMessages({
               name: {
                 id: 'field.media_bampfa.title.name',
-                defaultMessage: 'Title',
+                defaultMessage: 'Title IS RIGHT HERE',
               },
             }),
             view: {
