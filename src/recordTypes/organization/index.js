6765b1ffc8f499b7dfa5d92b1db84cbf8e0c5294
Ray Lee <rhlee@berkeley.edu>
2018-02-09 16:24:31 -0800

    BAMPFA-543: Remove unneeded form customizations.

--- src/recordTypes/organization/index.js
+++ src/recordTypes/organization/index.js
@@ -1,11 +1,9 @@
-import forms from './forms';
 import vocabularies from './vocabularies';
 
 export default pluginContext => ({
   recordTypes: {
     organization: {
       vocabularies,
-      forms: forms(pluginContext),
     },
   },
 });
