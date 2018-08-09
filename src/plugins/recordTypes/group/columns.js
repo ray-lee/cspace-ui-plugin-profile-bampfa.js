e001511f0b3bb0a4b4ae5f5251a39b4d1cc34fc3
Ray Lee <rhlee@berkeley.edu>
2018-08-09 15:05:10 -0700

    NOJIRA: Remove formatValue from group owner column definition.

---
+++ src/plugins/recordTypes/group/columns.js
@@ -0,0 +1,7 @@
+export default () => ({
+  default: {
+    owner: {
+      formatValue: null,
+    },
+  },
+});
--- src/plugins/recordTypes/group/index.js
+++ src/plugins/recordTypes/group/index.js
@@ -1,8 +1,10 @@
+import columns from './columns';
 import fields from './fields';
 
 export default () => configContext => ({
   recordTypes: {
     group: {
+      columns: columns(configContext),
       fields: fields(configContext),
     },
   },
