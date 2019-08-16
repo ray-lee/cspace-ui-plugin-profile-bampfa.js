6c31bfa1cf32a627397dd29717eae822e43c3065
cesarvh <cesarv.h@berkeley.edu>
2019-08-16 14:07:29 -0400

    CC-728: Added various parameters to UOCApprovalStatus report

--- src/plugins/invocables/report/bampfaUOCApprovalStatus/fields.js
+++ src/plugins/invocables/report/bampfaUOCApprovalStatus/fields.js
@@ -4,6 +4,10 @@ export default (configContext) => {
   const {
     CompoundInput,
     OptionPickerInput,
+    TermPickerInput,
+    DateInput,
+    TextInput,
+    AutocompleteInput,
   } = configContext.inputComponents;
 
   const {
@@ -32,6 +36,77 @@ export default (configContext) => {
             props: {
               source: 'uocApprovalStatusMIMETypes',
             },
+          },
+        },
+      },
+      Requester: {
+        [config]: {
+          messages: defineMessages({
+            name: {
+              id: 'field.report.bampfaUOCApprovalStatus.Requester.name',
+              defaultMessage: 'Requester',
+            },
+          }),
+          view: {
+            type: TextInput,
+          },
+        }
+      },
+      AuthBy: {
+        [config]: {
+          messages: defineMessages({
+            name: {
+              id: 'field.report.bampfaUOCApprovalStatus.AuthBy.name',
+              defaultMessage: 'Authorized by',
+            },
+          }),
+          view: {
+            type: AutocompleteInput,
+            props: {
+              source: 'person/local,person/shared,person/ulan,organization/local,organization/shared,organization/collective,organization/ulan',
+            },
+          },
+        },
+      },
+      AuthStatus: {
+        [config]: {
+          messages: defineMessages({
+            name: {
+              id: 'field.report.bampfaUOCApprovalStatus.AuthStatus.name',
+              defaultMessage: 'Authorization status',
+            },
+          }),
+          view: {
+            type: TermPickerInput,
+            props: {
+              source: 'uocauthorizationstatuses',
+            },
+          },
+        },
+      },
+      StartDate: {
+        [config]: {
+          messages: defineMessages({
+            name: {
+              id: 'field.report.bampfaUOCApprovalStatus.StartDate.name',
+              defaultMessage: 'Start date',
+            },
+          }),
+          view: {
+            type: DateInput,
+          },
+        },
+      },
+      EndDate: {
+        [config]: {
+          messages: defineMessages({
+            name: {
+              id: 'field.report.bampfaUOCApprovalStatus.EndDate.name',
+              defaultMessage: 'End date',
+            },
+          }),
+          view: {
+            type: DateInput,
           },
         },
       },
