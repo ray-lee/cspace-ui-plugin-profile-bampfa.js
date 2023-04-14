7341c2c89a571301cee7dcf9535addacf076dbd9
Cesar Villalobos <cesarv.h@berkeley.edu>
2023-04-14 16:57:26 -0400

    CSC-2199: Fixed ID Generator definition

--- src/plugins/recordTypes/conditioncheck/idGenerators.js
+++ src/plugins/recordTypes/conditioncheck/idGenerators.js
@@ -1,11 +1,11 @@
 import { defineMessages } from 'react-intl';
 
 export default {
-  bampfaconditioncheck: {
+  bampfaconditioncheckgenerator: {
     csid: '63a779aa-7d25-4af2-a2d9-66ce72fc70aa',
     messages: defineMessages({
       type: {
-        id: 'idGenerator.bampfaconditioncheck.type',
+        id: 'idGenerator.bampfaconditioncheckgenerator.type',
         defaultMessage: 'Condition Check',
       },
     }),
